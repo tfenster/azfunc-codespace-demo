2dba56d8d0fb7e4a805e0e1b1a983399da4465c2
Tobias Fenster <tfenster@4psbau.de>
2022-07-24 15:16:30 +0000

    implement logic

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -7,19 +7,57 @@ param($Request, $TriggerMetadata)
 Write-Host "PowerShell HTTP trigger function processed a request."
 
 # Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
+$vmname = $Request.Query.VmName
+
+if ($vmname) {
+    try {
+        Write-Host "Getting access token and connecting to Azure account"
+        $accessToken = ""
+        $clientId = ""
+        if (Test-Path env:\accesstoken) {
+            $accessToken = $env:accesstoken
+            $kvAccessToken = $env:kvaccesstoken
+            $clientId = $env:clientid
+        }
+        else {
+            $resourceURI = "https://management.core.windows.net/"
+            $tokenAuthURI = $env:IDENTITY_ENDPOINT + "?resource=$resourceURI&api-version=2019-08-01"
+            $tokenResponse = Invoke-RestMethod -Method Get -Headers @{"X-IDENTITY-HEADER" = "$env:IDENTITY_HEADER" } -Uri $tokenAuthURI
+            $accessToken = $tokenResponse.access_token
+            $clientId = $tokenResponse.client_id
+        }
+
+        Connect-AzAccount -AccessToken $accessToken -AccountId $clientId | Out-Null
+        $body = Get-AzVM -VMName "$vmname" -Status | Select-Object powerstate, id, vmid, name
+
+        if ($null -eq $body) {
+            Write-Host "no response"
+            Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                    StatusCode = [HttpStatusCode]::NotFound
+                })
+        }
+        
+        Write-Host "Success"
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode = [HttpStatusCode]::OK
+                Body       = $body
+            })   
+    }
+    catch {
+        Write-Host "Caught an exception: $($_.ToString())"
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode = [HttpStatusCode]::BadRequest
+                Body       = @{
+                    "Message"    = $_.ToString()
+                    "StackTrace" = $_.ScriptStackTrace
+                }
+            })
+    } 
 }
-
-$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-
-if ($name) {
-    $body = "Hello, $name. This HTTP triggered function executed successfully."
+else {
+    Write-Host "Didn't find the expected param"
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::BadRequest
+            Body       = "This HTTP triggered function executed successfully. Pass a VM name in the query string to get the PowerState."
+        })    
 }
-
-# Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body = $body
-})
